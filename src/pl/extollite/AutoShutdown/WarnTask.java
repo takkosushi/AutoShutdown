6bd00a69c9dacd506681e3cb56737c7b89d0f95b
takkosushi <63266905+takkosushi@users.noreply.github.com>
2020-04-22 17:32:16 +0200

    deutsch

--- src/pl/extollite/AutoShutdown/WarnTask.java
+++ src/pl/extollite/AutoShutdown/WarnTask.java
@@ -19,25 +19,25 @@ public class WarnTask extends TimerTask {
             if (TimeUnit.SECONDS.toMinutes(seconds) > 0L) {
                 if (TimeUnit.SECONDS.toMinutes(seconds) == 1L) {
                     if (seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds)) == 0L)
-                        plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in 1 minute...");
+                        plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in einer Minute...");
                     else {
-                        plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in 1 minute"+
-                                Long.valueOf(seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds))) +" seconds...");
+                        plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in einer Minute"+
+                                Long.valueOf(seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds))) +" Sekunden...");
                     }
 
                 } else if (seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds)) == 0L) {
                     plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in "+
-                            Long.valueOf(TimeUnit.SECONDS.toMinutes(seconds)) +" minutes...");
+                            Long.valueOf(TimeUnit.SECONDS.toMinutes(seconds)) +" Minuten...");
                 } else {
                     plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in "+
-                            Long.valueOf(TimeUnit.SECONDS.toMinutes(seconds)) +" minutes "+
-                            Long.valueOf(seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds))) +" seconds...");
+                            Long.valueOf(TimeUnit.SECONDS.toMinutes(seconds)) +" Minuten "+
+                            Long.valueOf(seconds - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(seconds))) +" Sekunden...");
                 }
 
             } else if (TimeUnit.SECONDS.toSeconds(seconds) == 1L)
-                plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " NOW!");
+                plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " Neustart!");
             else
-                plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in "+ Long.valueOf(seconds) +" seconds...");
+                plugin.getServer().broadcastMessage(TextFormat.RED + plugin.shutdownMessage + " in "+ Long.valueOf(seconds) +" Sekunden...");
         }, false);
     }
 }
